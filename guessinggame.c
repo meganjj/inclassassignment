72054ce02887f5ac0c11cb131826f2842803fbff
Megan <mjoo@cpp.edu>
2021-09-24 23:05:54 -0700

    Created guessing game file

---
+++ guessinggame.c
@@ -0,0 +1,6 @@
+/* Create a print statement that shows menu and use rand to choose a number between 1-10
+   take input from user and use if statements? for their inputs (1,2,or 3)
+   if 1 --> take input from user and create if statements for greater less and equal to the random number
+   but if q go back to menu..
+   if 2 --> take input from user to choose a max and check whether input is in bounds
+   if 3 --> print statement saying thank you and return? to end

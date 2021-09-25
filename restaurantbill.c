8943df78a2b82fbbc1f9dfb826dfa04321f68014
Megan <mjoo@cpp.edu>
2021-09-24 22:56:33 -0700

    Created file with outline

---
+++ restaurantbill.c
@@ -0,0 +1,4 @@
+/* create an array with the food item costs and a random number generator from 1-4 to choose one food
+   take in 2 arguments from the command line for tax and tip
+   calculate the tax and tip with the cost of the food and add them all together
+   display subtotal, tax, tip, and final total 
